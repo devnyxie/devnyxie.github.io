07a253fbb6e31db50affe2762652b1ad21cd0d63
Tim Afanasiev <84281349+devnyxie@users.noreply.github.com>
2024-06-06 00:08:57 +0200

    personal info update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,30 +12,19 @@ function ArrowIcon() {
         fill="currentColor"
       />
     </svg>
-  )
+  );
 }
 
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 gap-x-2 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/devnyxie"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -46,10 +35,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://www.linkedin.com/in/talmkg"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
       </ul>
@@ -57,5 +46,5 @@ export default function Footer() {
         © {new Date().getFullYear()} MIT Licensed
       </p>
     </footer>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,43 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
+import Image from "next/image";
+import pfp from "./other/pfp.jpg";
 
 export default function Page() {
+  const repos = [
+    { name: "next.js", url: "https://github.com/vercel/next.js" },
+    { name: "material-ui", url: "https://github.com/mui/material-ui" },
+    { name: "pterm", url: "https://github.com/pterm/pterm" },
+  ];
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">About</h1>
+      <div className="mb-4">
+        <Image
+          src={pfp}
+          alt="Profile Picture"
+          width={300}
+          height={300}
+          className="mb-2"
+        />
+        <p>
+          About I'm Tim, a passionate Software Developer skilled in both
+          Frontend and Backend development. I'm fluent in English, Greek,
+          Russian, and Polish.
+        </p>
+        <p>Skilled in TypeScript, Ruby, Python and Go.</p>
+        <div className="mt-3">
+          <p>Official collaborator at:</p>
+          <ul style={{ paddingLeft: "2rem" }}>
+            {repos.map((repo) => (
+              <li key={repo.name} style={{ listStyle: "initial" }}>
+                <a href={repo.url} target="_blank" rel="noopener noreferrer">
+                  {repo.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
-  )
+  );
 }
