3b6e90ddc28cdde0320f5f8b1ded15955f5f6520
Tim Afanasiev <84281349+devnyxie@users.noreply.github.com>
2024-06-27 18:22:57 +0200

    adj

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,15 +1,27 @@
 import { ImageResponse } from "next/og";
+import type { NextRequest } from "next/server";
 
-export function GET(request: Request) {
-  let url = new URL(request.url);
-  let title = url.searchParams.get("title") || "Next.js Portfolio Starter";
+export const runtime = "experimental-edge"; // Enables the Edge Runtime
+export const dynamic = "force-dynamic"; // Ensure this route is always treated as dynamic
+
+export function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const title = searchParams.get("title") || "Tim's Website";
 
   return new ImageResponse(
     (
-      <div>
-        <div>
-          <h2>{title}</h2>
-        </div>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#f0f0f0",
+          fontSize: 40,
+        }}
+      >
+        <h2>{title}</h2>
       </div>
     ),
     {
